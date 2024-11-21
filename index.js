ba80da5f6c2d7d007db5d8c356ac62a71d3181d6
Harrison-Furg <1fergusonhar@hdsb.ca>
2024-11-21 16:06:13 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,29 +1,20 @@
-/* You are making an app that keeps track of things in the supermarket
-Declare a variable under each comment below stating:
- 
-1) The most appropriate type 
-2) An appropriate name
-3) An initial value of your choosing 
+// Here is where you put all your code
 
-*/
+// The console.log() function will print whatever you
+// put in the brackets in the websites console
 
-console.log("Test!");
- 
-// The title of the app
-const title = "Supermarket Tracker"
-// The number of users who have downloaded the app
-let downloads = 567;
-// The number of apples in stock
-let apples = 262;
-// The number of bananas in stock.
-let bananas = 452;
-// The number of checkouts in the supermarket. 
-let checkouts = 15082;
-// The name of the warehouse worker working today 
-let worker = "Samuel Lucas";
 
-console.log("Welcome to " + title + "!");
-console.log("Downloads: " + downloads + ".");
-console.log("Today, " + worker + " is working in warehouse.");
-console.log("There are " + apples + " apples and " + bananas + " bananas.");
-console.log("a total of " + checkouts + " checkouts have been completed today.")
+
+
+// Variables can be declaired to hold numbers or words.
+// They can be put into the console.log() functions to be printed
+
+
+
+
+// If() functions check to see if the calculations within the 
+// brackets are true or not and activate based on that
+
+
+
+
