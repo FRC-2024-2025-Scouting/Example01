6ae54602a00442e9849b82ebfbc85b9fd35fcded
Harrison-Furg <1fergusonhar@hdsb.ca>
2024-11-21 15:15:47 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,29 @@
+/* You are making an app that keeps track of things in the supermarket
+Declare a variable under each comment below stating:
+ 
+1) The most appropriate type 
+2) An appropriate name
+3) An initial value of your choosing 
+
+*/
+
+console.log("Test!");
+ 
+// The title of the app
+const title = "Supermarket Tracker"
+// The number of users who have downloaded the app
+let downloads = 567;
+// The number of apples in stock
+let apples = 262;
+// The number of bananas in stock.
+let bananas = 452;
+// The number of checkouts in the supermarket. 
+let checkouts = 15082;
+// The name of the warehouse worker working today 
+let worker = "Samuel Lucas";
+
+console.log("Welcome to " + title + "!");
+console.log("Downloads: " + downloads + ".");
+console.log("Today, " + worker + " is working in warehouse.");
+console.log("There are " + apples + " apples and " + bananas + " bananas.");
+console.log("a total of " + checkouts + " checkouts have been completed today.")
