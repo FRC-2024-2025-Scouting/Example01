19a3ad4253f6dbf9463c2a5dbf49b0051af74312
Harrison-Furg <1fergusonhar@hdsb.ca>
2024-11-21 16:41:17 -0500

    Added vars logs and functions

--- index.js
+++ index.js
@@ -3,18 +3,46 @@
 // The console.log() function will print whatever you
 // put in the brackets in the websites console
 
+console.log("Hello World!");
 
 
 
 // Variables can be declaired to hold numbers or words.
 // They can be put into the console.log() functions to be printed
 
+let int = 1;
+let string = 'This is a string';
+let bool = true;
+let float = 2.1;
 
+console.log(int + ' this is the int');
+console.log(string);
+console.log(bool);
+console.log(float + int);
 
 
 // If() functions check to see if the calculations within the 
 // brackets are true or not and activate based on that
 
+if (int == 1) {
+    console.log("int is equal to 1")
+    int = 2;
+} 
+
+if (int == 2) {
+    console.log('int is equal to 2')
+} else {
+    console.log('int is not equal to 2')
+}
+
+function test(num) {
+    console.log(num);
+};
+
+test('this is a function');
 
 
-
+//Define 5 var
+//Log those 5 Var
+//Make 2 if statements that modify 2 variables each
+//re log those variables
